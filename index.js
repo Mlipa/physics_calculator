10ef4d78fa20b837d00c143e528788187ad3e31e
Miguel Lipa <miguellipa1@gmail.com>
2021-09-23 20:39:10 -0400

    Calculo circulo

--- index.js
+++ index.js
@@ -12,3 +12,20 @@ const calcularPerimetroCuadrado = () => {
   const resultado = PerimetroDelCuadrado(LadoDelCuadrado);
   document.querySelector("#resultado").innerHTML = resultado + " cm";
 };
+
+// Calculo del Circulo
+
+const PerimetroDelCirculo = (radio) => (2*Math.PI*radio).toFixed(3);
+const AreaDelCirculo = (radio)=> (Math.PI*Math.pow(radio,2)).toFixed(3);
+
+const CalculoAreaCirculoculo = ()=> {
+    const radio = document.querySelector("#radio-circulo").value;
+    const resultado = AreaDelCirculo(radio);
+    document.querySelector("#resultado").innerHTML=resultado + " cm";
+}
+
+const CalculoPerimetroCirculo = ()=> {
+    const radio = document.querySelector("#radio-circulo").value;
+    const resultado = PerimetroDelCirculo(radio);
+    document.querySelector("#resultado").innerHTML=resultado + " cm";
+}
