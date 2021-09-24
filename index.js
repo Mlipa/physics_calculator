e42db2d78d8b11c12dc878c620f047a21d6a0856
Jefferson Gutierrez <tonyguti425@gmail.com>
2021-09-23 20:43:17 -0400

    Calculo de Hexagono Terminado

--- index.js
+++ index.js
@@ -12,3 +12,26 @@ const calcularPerimetroCuadrado = () => {
   const resultado = PerimetroDelCuadrado(LadoDelCuadrado);
   document.querySelector("#resultado").innerHTML = resultado + " cm";
 };
+
+
+
+
+
+
+
+
+//Calculo del Hexagono
+const AreaDelHexagono = (LadoDelHexagono) => (((3*Math.sqrt(3))*Math.pow(LadoDelHexagono,2))/2).toFixed(4);
+const PerimetroDelHexagono= (LadoDelHexagono) => LadoDelHexagono * 6;
+
+const calcularAreaHexagono = () => {
+  const LadoDelHexagono = document.querySelector("#lado-hexagono").value;
+  const resultado = AreaDelHexagono(LadoDelHexagono);
+  document.querySelector("#resultado").innerHTML = resultado + " cm";
+};
+
+const calcularPerimetroHexagono = () => {
+  const LadoDelHexagono = document.querySelector("#lado-hexagono").value;
+  const resultado = PerimetroDelHexagono(LadoDelHexagono);
+  document.querySelector("#resultado").innerHTML = resultado + " cm";
+};
